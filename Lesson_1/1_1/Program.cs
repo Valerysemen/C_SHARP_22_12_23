17caace5146ecfcdfbc3a0162410e42836ca4f18
Valery <6488671s@gmail.com>
2024-01-08 22:20:28 +0300

    Lesson1 task1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter a number 1: ");
+int b1 = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Enter a number 2: ");
+int b2 = Convert.ToInt32(Console.ReadLine()!);
+
+if (b2 * b2 == b1)
+Console.WriteLine("Yes");
+else
+Console.WriteLine("No");
