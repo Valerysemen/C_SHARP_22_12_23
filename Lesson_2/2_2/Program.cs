657a4d9029c23f2ac37eaf1d4231ff2bb38fac21
Valery <6488671s@gmail.com>
2024-01-09 16:31:30 +0300

    Lesson 2 task1 task2

---
+++ Lesson_2/2_2/Program.cs
@@ -0,0 +1,26 @@
+﻿//2. Напишите программу, которая принимает на вход трёхзначное
+// число и возводит вторую цифру этого числа в степень, равную
+// третьей цифре.
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num < 1000)
+{
+    int num1 = num % 100 / 10;
+    int num2 = num % 10;
+    double result = Math.Pow(num1, num2);
+    Console.WriteLine(result);
+}
+else
+{
+    if (num < 100 || num > 1000)
+    {
+        Console.WriteLine("Вы ввели не трехзначное число");
+    }
+}
+
+// Вариант 2
+
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Math.Pow(number / 10 % 10, number % 10));
