459740e68ca718ba91a36e87d1eb1775aa794ccc
Valery <6488671s@gmail.com>
2024-01-13 19:39:14 +0300

    add lesson2:taskHW4

---
+++ Lesson_2/2HW/2HW_4/Program.cs
@@ -0,0 +1,16 @@
+﻿//  Напишите программу, которая на вход принимает натуральное число N,
+//а на выходе показывает его цифры через запятую.
+
+Console.Write("Ввод натурального числа N ");
+int num = int.Parse(Console.ReadLine()!);
+
+string numStr = num.ToString();
+for (int i = 0; i < numStr.Length; i++)
+{
+    Console.Write(numStr[i]);
+
+    if (i < numStr.Length - 1)
+    {
+        Console.Write(", ");
+    }
+}
